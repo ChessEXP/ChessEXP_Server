f530af157d1d505638741b6879b79b402276001b
Andrew Eskenazi <drewesk@gmail.com>
2017-08-02 21:46:56 -0600

    First Commit 🍜

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const auth = require('./auth/user');
+const article = require('./routes/article');
+// const users = require('./routes/users');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/auth', auth);
+app.use('/users', users);
+// app.use('/', article);
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: `${err.message} ☔️`,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
+});
+
+module.exports = app;
---
+++ db/queries.js
@@ -0,0 +1,19 @@
+const knex = require('../connection');
+
+module.exports = {
+
+  getAllArticles: () => {
+    return knex("article");
+  },
+  getOneArticle: (id) => {
+    return knex("article").where('id', id).first();
+  },
+  createArticle: (article) => {
+    return knex('article').insert(article);
+  },
+  updateArticle: (article_id, article) => {
+    return knex('article').where('id', article_id).update(article, '*');
+  },
+  deleteArticle: (article_id) => {
+    return knex('article').where('id', article_id).del();
+  }
